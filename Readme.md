71fec05417a4eeeb5bb739b8d65a5a6c5d8cb447
CismonX <qiaocheng@twt.edu.cn>
2017-07-12 22:22:18 +0800

    Add full compatibility to Amp.

--- Readme.md
+++ Readme.md
@@ -6,22 +6,16 @@
 
 本项目用于将`Amp`的event-loop应用于`Workerman`，从而可以在`Workerman`中使用基于`Amp`的高性能组件，例如异步`MySQL`，异步`Redis`，异步HTTP客户端等。
 
+笔者修改了本项目的源码，使其兼容原生`Amp`。现在无需修改`Amp`的源码就可以在项目中使用其所有组件。以后笔者可能会发布一些Demo。
+
 ### 使用说明
 
-1. 将`src`目录下的`Amp.php`复制到`Workerman\Events`下。
+1. 将`src`目录下的`Amp.php`复制到`Workerman\Events`下，或者你可以选择使用其他方式加载。
 
-2. 将目录`Amp`及其包含的所有文件复制到项目目录中可以被自动加载的位置下（PSR-4规范）。
+2. 将`Amp`加载到当前项目中。（建议使用composer管理依赖）
 
 3. 将`Amp`设置为`Workerman`所使用的event-loop。如下：
 
 ```php
 Worker::$eventLoopClass = '\\Workerman\\Events\\Amp';
 ```
-
-### 注意
-
-1. 本项目中使用的`Amp`是笔者略加修改的版本。由于`Amp`中的回调函数的首个参数是`$watcher_id`而不是`$fd`，所以不可以直接将`Workerman`中的事件回调作为Watcher。因此笔者修改了部分Watcher回调的传参顺序，使其与`Workerman`兼容。这样做的后果是不能保证其他`Amp`组件的可用性。在使用这些组件前需要检查其源码并根据情况作出相应修改。
-
-2. 笔者还移除了`Amp\Promise`对`React\Promise\PromiseInterface`的支持。如果有需要，开发者可以自行添加回来。以上所有对Amp源码的修改，可以与[官方仓库的master分支](https://github.com/amphp/amp)作对比。
-
-3. 笔者仅对本项目进行过少量测试，不能保证其稳定性。请避免将其应用于生产环境。
--- src/Amp.php
+++ src/Amp.php
@@ -3,38 +3,39 @@ namespace Workerman\Events;
 use Amp\Loop;
 use Workerman\Worker;
 
-class Amp implements EventInterface
-{
+class Amp implements EventInterface {
 
     protected $_allEvents = [];
-
     protected $_eventSignal = [];
-
     protected $_eventTimer = [];
-
     protected static $_timerId = 1;
 
-    public function add($fd, $flag, $func, $args = array())
-    {
+    public function add($fd, $flag, $func, $args = []) {
         $args = (array)$args;
         switch ($flag) {
-            case EventInterface::EV_READ:
+            case self::EV_READ:
                 $fd_key = intval($fd);
-                $event = Loop::onReadable($fd, $func);
+                $event = Loop::onReadable($fd, function ($id, $socket) use ($func) {
+                    $func($socket);
+                });
                 $this->_allEvents[$fd_key][$flag] = $event;
                 return true;
-            case EventInterface::EV_WRITE:
+            case self::EV_WRITE:
                 $fd_key = intval($fd);
-                $event = Loop::onWritable($fd, $func);
+                $event = Loop::onWritable($fd, function ($id, $socket) use ($func) {
+                    $func($socket);
+                });
                 $this->_allEvents[$fd_key][$flag] = $event;
                 return true;
-            case EventInterface::EV_SIGNAL:
+            case self::EV_SIGNAL:
                 $fd_key = intval($fd);
-                $event = Loop::onSignal($fd, $func);
+                $event = Loop::onSignal($fd, function ($id, $signal) use ($func) {
+                    $func($signal);
+                });
                 $this->_eventSignal[$fd_key] = $event;
                 return true;
-            case EventInterface::EV_TIMER:
-            case EventInterface::EV_TIMER_ONCE:
+            case self::EV_TIMER:
+            case self::EV_TIMER_ONCE:
                 $param = [$func, (array)$args, $flag, self::$_timerId];
                 $event = Loop::repeat($fd * 1000, \Closure::bind(function () use ($param) {
                     $timer_id = $param[3];
@@ -60,11 +61,10 @@ class Amp implements EventInterface
         return false;
     }
 
-    public function del($fd, $flag)
-    {
+    public function del($fd, $flag) {
         switch ($flag) {
-            case EventInterface::EV_READ:
-            case EventInterface::EV_WRITE:
+            case self::EV_READ:
+            case self::EV_WRITE:
                 $fd_key = intval($fd);
                 if (isset($this->_allEvents[$fd_key][$flag])) {
                     Loop::cancel($this->_allEvents[$fd_key][$flag]);
@@ -73,15 +73,15 @@ class Amp implements EventInterface
                 if (empty($this->_allEvents[$fd_key]))
                     unset($this->_allEvents[$fd_key]);
                 break;
-            case EventInterface::EV_SIGNAL:
+            case self::EV_SIGNAL:
                 $fd_key = intval($fd);
                 if (isset($this->_eventSignal[$fd_key])) {
                     Loop::cancel($this->_eventSignal[$fd_key]);
                     unset($this->_eventSignal[$fd_key]);
                 }
                 break;
-            case EventInterface::EV_TIMER:
-            case EventInterface::EV_TIMER_ONCE:
+            case self::EV_TIMER:
+            case self::EV_TIMER_ONCE:
                 if (isset($this->_eventTimer[$fd])) {
                     Loop::cancel($this->_eventTimer[$fd]);
                     unset($this->_eventTimer[$fd]);
@@ -91,20 +91,17 @@ class Amp implements EventInterface
         return true;
     }
 
-    public function loop()
-    {
+    public function loop() {
         Loop::run();
     }
 
-    public function clearAllTimer()
-    {
+    public function clearAllTimer() {
         foreach ($this->_eventTimer as $event)
             Loop::cancel($event);
         $this->_eventTimer = [];
     }
 
-    public function destroy()
-    {
+    public function destroy() {
         foreach ($this->_eventSignal as $event)
             Loop::cancel($event);
     }
